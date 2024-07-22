25c677f2e7f602c0356af722fbe70e17ce60b4f7
Mouayad <84500993+Mouayad91@users.noreply.github.com>
2024-07-22 21:38:17 +0200

    first commit

---
+++ src/main/java/com/banking_app/banking_app/entity/Account.java
@@ -0,0 +1,36 @@
+package com.banking_app.banking_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="accounts")
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="accoount_owner_name")
+    private String accountOwnerName;
+
+    @Column(name="balance")
+    private double balance;
+
+}
